a9336bd5c2525bf9d0461fb93adc035477ea043b
azurity <784227594@qq.com>
2019-09-27 22:38:41 +0800

    init

---
+++ server/src/main/java/club/tp0t/oj/OjApplication.java
@@ -0,0 +1,13 @@
+package club.tp0t.oj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjApplication.class, args);
+	}
+
+}
