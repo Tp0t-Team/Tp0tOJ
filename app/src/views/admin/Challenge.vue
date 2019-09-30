8ba5f0e2e3861f55a26bad071bff9a19126aa3c7
azurity <784227594@qq.com>
2019-10-01 00:09:45 +0800

    2019年10月1日00:09:42

--- app/src/views/admin/Challenge.vue
+++ app/src/views/admin/Challenge.vue
@@ -2,26 +2,28 @@
   <v-container fluid>
     <v-row>
       <v-col cols="6">
-        <v-list dense>
-          <v-subheader>WEB</v-subheader>
-          <v-list-item @click=";">
-            <v-list-item-content>123</v-list-item-content>
-          </v-list-item>
-          <v-list-item @click=";">
-            <v-list-item-content>123</v-list-item-content>
-          </v-list-item>
-          <v-list-item @click=";">
-            <v-list-item-content>123</v-list-item-content>
-          </v-list-item>
-          <v-list-item @click=";">
-            <v-list-item-content>123</v-list-item-content>
-          </v-list-item>
-        </v-list>
+        <v-card>
+          <v-toolbar dense>web</v-toolbar>
+          <v-list dense>
+            <v-list-item @click=";">
+              <v-list-item-content>123</v-list-item-content>
+            </v-list-item>
+            <v-list-item @click=";">
+              <v-list-item-content>123</v-list-item-content>
+            </v-list-item>
+            <v-list-item @click=";">
+              <v-list-item-content>123</v-list-item-content>
+            </v-list-item>
+            <v-list-item @click=";">
+              <v-list-item-content>123</v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
       </v-col>
       <v-col cols="6">
-          <v-form>
-              <v-text-field disabled value="web"></v-text-field>
-          </v-form>
+        <v-form>
+          <v-text-field disabled value="web"></v-text-field>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
