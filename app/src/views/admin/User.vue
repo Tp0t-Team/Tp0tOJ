55ce5cca02b6bc701ed241b3d133aba1cfc2c49b
azurity <784227594@qq.com>
2019-10-05 17:01:24 +0800

    2019年10月5日17:01:19

--- app/src/views/admin/User.vue
+++ app/src/views/admin/User.vue
@@ -152,7 +152,7 @@ export default class User extends Vue {
         { userId: string }
       >({
         query: gql`
-          query($userId: string) {
+          query($userId: String!) {
             submitHistory(userId: $userId) {
               message
               submitInfos {
