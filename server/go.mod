dc2bedff52d4dc893ac41c92e9e83f8e8e891685
LordCasser <591289332@qq.com>
2022-02-07 22:51:01 +0800

    init files

---
+++ server/go.mod
@@ -0,0 +1,10 @@
+module server
+
+go 1.16
+
+require (
+	github.com/jackc/pgx/v4 v4.14.1 // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	gorm.io/driver/postgres v1.2.3
+	gorm.io/gorm v1.22.5
+)
---
+++ server/utils/postgres.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+//dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+//db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
