15dcbedb4c5800363ecf15c728827851a7c7e161
LordCasser <591289332@qq.com>
2022-02-08 01:03:56 +0800

    tested and update all sql entity

--- server/entity/Submit.go
+++ server/entity/Submit.go
@@ -3,14 +3,15 @@ package entity
 import "time"
 
 type Submit struct {
-	SubmitId   uint64    `gorm:"primaryKey"`
-	UserId     uint64    `gorm:"not null"`
-	User       User      `gorm:"foreignKey:UserId"`
-	Mark       int64     `gorm:"not null"`
-	SubmitTime time.Time `gorm:"not null"`
-	Flag       string    `gorm:"check: flag <> ''"`
-	Correct    bool
-	CreatedAt  time.Time `gorm:"autoCreateTime;not null;"`
-	UpdatedAt  time.Time `gorm:"autoUpdateTime;not null;"`
-	Challenge  Challenge `gorm:"foreignKey:Challenge;not null;"`
+	SubmitId    uint64    `gorm:"primaryKey"`
+	UserId      uint64    `gorm:"not null"`
+	User        User      `gorm:"foreignKey:UserId"`
+	Mark        int64     `gorm:"not null"`
+	SubmitTime  time.Time `gorm:"not null"`
+	Flag        string    `gorm:"check: flag <> ''"`
+	Correct     bool
+	CreatedAt   time.Time `gorm:"autoCreateTime;not null;"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime;not null;"`
+	ChallengeId uint64    `gorm:"not null"`
+	Challenge   Challenge `gorm:"foreignKey:ChallengeId"`
 }
