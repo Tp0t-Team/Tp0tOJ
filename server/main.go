46c3917d31d1c0e31b76a65babb99da9cce2c0c4
LordCasser <591289332@qq.com>
2022-03-01 23:31:20 +0800

    add kube

--- server/main.go
+++ server/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	// TODO: provide --prepare flags to check environment and install k3s server and other requirement
+
 	config := new(utils.Config)
 	utils.Parse(config)
 	//http.Handle("")
