1dfa51568a6d339e0554e8a2c94335bf3b98f17b
azurity <784227594@qq.com>
2022-03-02 22:48:46 +0800

    a special find

--- server/services/database/resolvers/ReplicaAlloc.go
+++ server/services/database/resolvers/ReplicaAlloc.go
@@ -19,6 +19,33 @@ func FindReplicaAllocByUserId(userId uint64) []entity.ReplicaAlloc {
 	return replicaAllocs
 }
 
+func FindReplicaAllocByUserIdAndChallengeId(userId uint64, challengeId uint64, outsideTX *gorm.DB) (*entity.ReplicaAlloc, error) {
+	if outsideTX == nil {
+		outsideTX = db
+	}
+	found := []entity.ReplicaAlloc{}
+	err := db.Transaction(func(tx *gorm.DB) error {
+		var allocs []entity.ReplicaAlloc
+		allocResult := tx.Where(map[string]interface{}{"UserId": userId}).Find(&allocs)
+		if allocResult.Error != nil {
+			return allocResult.Error
+		}
+		for _, alloc := range allocs {
+			if alloc.Replica.ChallengeId == challengeId {
+				found = append(found, alloc)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(found) != 1 {
+		return nil, errors.New("repeat replica alloc")
+	}
+	return &found[0], nil
+}
+
 func AddReplicaAlloc(replicaId uint64, userId uint64) bool {
 	// TODO: maybe need some more check, to ensure user only have one replica for each challenge
 	replicaAlloc := entity.ReplicaAlloc{
--- server/services/database/resolvers/Submit.go
+++ server/services/database/resolvers/Submit.go
@@ -59,40 +59,34 @@ func FindSubmitCorrectSorted() []entity.Submit {
 
 func AddSubmit(userId uint64, challengeId uint64, flag string, submitTime time.Time) bool {
 	err := db.Transaction(func(tx *gorm.DB) error {
-		var allocs []entity.ReplicaAlloc
-		allocResult := tx.Where(map[string]interface{}{"UserId": userId}).Find(&allocs)
-		if allocResult.Error != nil {
-			return allocResult.Error
+		alloc, err := FindReplicaAllocByUserIdAndChallengeId(userId, challengeId, tx)
+		if err != nil {
+			return err
 		}
-		for _, alloc := range allocs {
-			if alloc.Replica.ChallengeId != challengeId {
-				continue
-			}
-			var submits []entity.Submit
-			result := tx.Where(map[string]interface{}{"ChallengeId": challengeId, "Correct": true, "Available": true}).Find(&submits)
-			if result.Error != nil {
-				return result.Error
-			}
-			var config types.ChallengeConfig
-			err := json.Unmarshal([]byte(alloc.Replica.Challenge.Configuration), &config)
-			if err != nil {
-				return err
-			}
-			if alloc.Replica.ChallengeId == challengeId {
-				newSubmit := entity.Submit{
-					UserId:      userId,
-					ChallengeId: challengeId,
-					SubmitTime:  submitTime,
-					Mark:        int64(len(submits)),
-					Flag:        flag,
-					Correct:     alloc.Replica.Flag == flag,
-					Available:   config.State == "enabled",
-				}
-				tx.Create(&newSubmit)
-				return nil
-			}
+		if alloc == nil {
+			return errors.New("no alloc exists")
 		}
-		return errors.New("no alloc exists")
+		var submits []entity.Submit
+		result := tx.Where(map[string]interface{}{"ChallengeId": challengeId, "Correct": true, "Available": true}).Find(&submits)
+		if result.Error != nil {
+			return result.Error
+		}
+		var config types.ChallengeConfig
+		err = json.Unmarshal([]byte(alloc.Replica.Challenge.Configuration), &config)
+		if err != nil {
+			return err
+		}
+		newSubmit := entity.Submit{
+			UserId:      userId,
+			ChallengeId: challengeId,
+			SubmitTime:  submitTime,
+			Mark:        int64(len(submits)),
+			Flag:        flag,
+			Correct:     alloc.Replica.Flag == flag,
+			Available:   config.State == "enabled",
+		}
+		tx.Create(&newSubmit)
+		return nil
 	})
 	if err != nil {
 		log.Println(err)
