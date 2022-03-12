9d0d28e5dd7463cd4ed9a9c3ecd38a73c639458c
azurity <784227594@qq.com>
2022-03-12 22:02:31 +0800

    fix unpack script

--- server/unpack.sh
+++ server/unpack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ARCHIVE=$(awk '/^__CONFIG_BELOW__/ {print NR + 1; exit 0; }' "$0")
-tail -n+$ARCHIVE "$0" > registries-config.yaml
+ARCHIVE=$(awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' "$0")
+tail -n+$ARCHIVE "$0" >  tar -ixzvm -C . > /dev/null 2>&1 3>&1
 if [ $? -ne 0 ];then
         echo "get config fail"
         exit 1
