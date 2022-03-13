438130c4a33257d86ab82601e9a655ce817f4805
azurity <784227594@qq.com>
2022-03-13 20:50:15 +0800

    fix x509 SAN

--- server/prepare.go
+++ server/prepare.go
@@ -9,7 +9,6 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	_ "embed"
-	"encoding/asn1"
 	"encoding/json"
 	"encoding/pem"
 	"flag"
@@ -19,6 +18,7 @@ import (
 	"log"
 	"math/big"
 	rd "math/rand"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -275,34 +275,45 @@ func CreateCert(masterIP string) {
 		os.Exit(1)
 	}
 
-	extSubjectAltName := pkix.Extension{}
-	extSubjectAltName.Id = asn1.ObjectIdentifier{2, 5, 29, 17}
-	extSubjectAltName.Critical = false
-	extSubjectAltName.Value = []byte(fmt.Sprintf("IP:%s", masterIP))
+	//extSubjectAltName := pkix.Extension{}
+	//extSubjectAltName.Id = asn1.ObjectIdentifier{2, 5, 29, 17}
+	//extSubjectAltName.Critical = false
+	//extSubjectAltName.Value = []byte(fmt.Sprintf("IP:%s", masterIP))
 
 	CACert := &x509.Certificate{
 		SerialNumber:          big.NewInt(rd.Int63()),
 		BasicConstraintsValid: true,
 		NotBefore:             time.Now(),
 		NotAfter:              time.Now().AddDate(1, 0, 0),
-		Subject:               pkix.Name{},
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-		EmailAddresses:        []string{},
-		ExtraExtensions:       []pkix.Extension{extSubjectAltName},
+		Subject: pkix.Name{
+			CommonName: "ca-" + masterIP,
+		},
+		Issuer: pkix.Name{
+			CommonName: "ca-" + masterIP,
+		},
+		IsCA: true,
+		//ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		//KeyUsage:       x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
+		EmailAddresses: []string{},
+		//ExtraExtensions: []pkix.Extension{extSubjectAltName},
 	}
 	Cert := &x509.Certificate{
 		SerialNumber:          big.NewInt(rd.Int63()),
 		BasicConstraintsValid: true,
 		NotBefore:             time.Now(),
 		NotAfter:              time.Now().AddDate(1, 0, 0),
-		Subject:               pkix.Name{},
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-		EmailAddresses:        []string{},
-		ExtraExtensions:       []pkix.Extension{extSubjectAltName},
+		Subject: pkix.Name{
+			CommonName: "ca-" + masterIP,
+		},
+		Issuer: pkix.Name{
+			CommonName: "ca-" + masterIP,
+		},
+		IsCA:           false,
+		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:       x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
+		EmailAddresses: []string{},
+		//ExtraExtensions: []pkix.Extension{extSubjectAltName},
+		IPAddresses: []net.IP{net.ParseIP(masterIP)},
 	}
 	Key, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
@@ -320,7 +331,8 @@ func CreateCert(masterIP string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	_, err = caFile.Write(buf)
+	err = pem.Encode(caFile, &pem.Block{Type: "CERTIFICATE", Bytes: buf})
+	//_, err = caFile.Write(buf)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -330,6 +342,9 @@ func CreateCert(masterIP string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	//CACert, _ = x509.ParseCertificate(buf)
+	buf = nil
 	buf, err = x509.CreateCertificate(rand.Reader, Cert, CACert, &Key.PublicKey, Key)
 	certFile, err := os.Create("resources/tls.crt")
 	if err != nil {
@@ -347,13 +362,14 @@ func CreateCert(masterIP string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	buf = nil
 	buf = x509.MarshalPKCS1PrivateKey(Key)
 	keyFile, err := os.Create("resources/tls.key")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	err = pem.Encode(keyFile, &pem.Block{Type: "PRIVATE KEY", Bytes: buf})
+	err = pem.Encode(keyFile, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: buf})
 	//_, err = keyFile.Write(buf)
 	if err != nil {
 		fmt.Println(err)
