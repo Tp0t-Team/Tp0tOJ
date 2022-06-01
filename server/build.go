faf6481a1920e564f65eeb4200725127bc062fcf
azurity <784227594@qq.com>
2022-06-01 10:27:36 +0800

    22-06-01 10:27

--- server/build.go
+++ server/build.go
@@ -99,14 +99,12 @@ func main() {
 
 	serverCmd := exec.Command("go", "build", "-tags", "WithFrontEnd", "-o", fmt.Sprintf("OJ_%s_%s", runtime.GOOS, runtime.GOARCH), "main.go")
 	log.Println("build server...")
-	errLog, _ := serverCmd.StderrPipe()
+	serverCmd.Stdout = os.Stdout
+	serverCmd.Stderr = os.Stderr
 	err = serverCmd.Run()
 	if err != nil {
-		io.Copy(os.Stdout, errLog)
-		errLog.Close()
 		log.Panicln(err)
 	}
-	errLog.Close()
 
 	err = os.RemoveAll("services/static")
 	if err != nil {
