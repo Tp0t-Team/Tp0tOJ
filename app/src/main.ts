7886b5566e596c493b2560ae718971841486adf7
LordCasser <591289332@qq.com>
2022-08-09 18:06:03 +0800

    debug

--- app/src/main.ts
+++ app/src/main.ts
@@ -8,20 +8,20 @@ import '@/main.scss'
 
 Vue.config.productionTip = false
 
-// router.beforeEach((to, from, next) => {
-//     let validator =
-//         typeof to.meta!.auth != 'string' ||
-//         (to.meta!.auth == 'member' && !!sessionStorage.getItem('user_id')) ||
-//         (to.meta!.auth == 'team' &&
-//             sessionStorage.getItem('role') != 'member') ||
-//         (to.meta!.auth == 'admin' && sessionStorage.getItem('role') == 'admin')
-//     let result = validator
-//         ? {}
-//         : {
-//               name: 'home'
-//           }
-//     next(result)
-// })
+router.beforeEach((to, from, next) => {
+    let validator =
+        typeof to.meta!.auth != 'string' ||
+        (to.meta!.auth == 'member' && !!sessionStorage.getItem('user_id')) ||
+        (to.meta!.auth == 'team' &&
+            sessionStorage.getItem('role') != 'member') ||
+        (to.meta!.auth == 'admin' && sessionStorage.getItem('role') == 'admin')
+    let result = validator
+        ? {}
+        : {
+              name: 'home'
+          }
+    next(result)
+})
 
 new Vue({
     vuetify,
